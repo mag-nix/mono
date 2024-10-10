e170ffe3b477161226c22063e7f053c75c8b6594
Johannes Jeising <jeising@magazino.eu>
2024-10-10 15:32:34 +0200

    add std-msgs package locally

---
+++ pkgs/std-msgs/default.nix
@@ -0,0 +1,30 @@
+
+# Copyright 2024 Open Source Robotics Foundation
+# Distributed under the terms of the BSD license
+
+{ lib, buildRosPackage, fetchurl, catkin, message-generation, message-runtime }:
+buildRosPackage {
+  pname = "ros-noetic-std-msgs";
+  version = "0.5.13-r1";
+
+  src = lib.fileset.toSource {
+    root = ./.;
+    fileset = lib.fileset.unions [
+      ./msg
+      ./include
+      ./CMakeLists.txt
+      ./package.xml
+    ];
+  };
+
+  buildType = "catkin";
+  buildInputs = [ catkin message-generation ];
+  propagatedBuildInputs = [ message-runtime ];
+  nativeBuildInputs = [ catkin ];
+
+  meta = {
+    description = "Standard ROS Messages including common message types representing primitive data types and other basic message constructs, such as multiarrays.
+    For common, generic robot-specific message types, please see <a href=\"http://www.ros.org/wiki/common_msgs\">common_msgs</a>.";
+    license = with lib.licenses; [ bsdOriginal ];
+  };
+}
